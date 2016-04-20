36ef3f4f493dd966f7ee2717f0814065adbee4e3
Sam Smith <sam@example.com>
2016-04-20 17:39:21 +0530

    Initial Commit

---
+++ NewProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NewProject
+//
+//  Created by Aseem 24 on 20/04/16.
+//  Copyright © 2016 Aseem 24. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
